d16495ed8916e94e27b20ad348d4e3b3b181acce
Kelly Dwan <kelly.dwan@gmail.com>
2016-12-29 12:25:49 -0500

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,67 @@
+/**
+ * External dependencies
+ */
+import { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import debugFactory from 'debug';
+
+/**
+ * Internal dependencies
+ */
+import { isRequestingUser } from './selectors';
+import { requestUser } from './state';
+
+const debug = debugFactory( 'query:users' );
+
+class QueryUser extends Component {
+	componentWillMount() {
+		this.request( this.props );
+	}
+
+	componentWillReceiveProps( nextProps ) {
+		if ( true ) {
+			return;
+		}
+
+		this.request( nextProps );
+	}
+
+	request( props ) {
+		if ( ! props.requestingUser ) {
+			debug( `Request user ${ props.userId }` );
+			props.requestUser( props.userId );
+		}
+	}
+
+	render() {
+		return null;
+	}
+}
+
+QueryUser.propTypes = {
+	userId: PropTypes.oneOfType( [
+		PropTypes.string,
+		PropTypes.number
+	] ).required,
+	requestingUser: PropTypes.bool,
+	requestUser: PropTypes.func
+};
+
+QueryUser.defaultProps = {
+	requestUser: () => {}
+};
+
+export default connect(
+	( state, ownProps ) => {
+		const { userId } = ownProps;
+		return {
+			requestingUser: isRequestingUser( state, userId ),
+		};
+	},
+	( dispatch ) => {
+		return bindActionCreators( {
+			requestUser
+		}, dispatch );
+	}
+)( QueryUser );
